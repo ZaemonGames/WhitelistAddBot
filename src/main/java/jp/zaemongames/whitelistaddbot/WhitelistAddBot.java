402ea81fc2ffe8f162910fa681428cf6741b980c
ZaemonGames <naokikun0904@gmail.com>
2023-03-24 20:07:43 +0900

    test

---
+++ src/main/java/jp/zaemongames/whitelistaddbot/WhitelistAddBot.java
@@ -0,0 +1,17 @@
+package jp.zaemongames.whitelistaddbot;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class WhitelistAddBot extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
