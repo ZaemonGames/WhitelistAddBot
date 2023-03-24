3b4bdce02870d9373faeddf940d89a16114885bf
ZaemonGames <naokikun0904@gmail.com>
2023-03-25 08:31:52 +0900

    addWhitelistAddMethod

--- src/main/java/jp/zaemongames/whitelistaddbot/WhitelistAddBot.java
+++ src/main/java/jp/zaemongames/whitelistaddbot/WhitelistAddBot.java
@@ -1,5 +1,6 @@
 package jp.zaemongames.whitelistaddbot;
 
+import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.OnlineStatus;
 import net.dv8tion.jda.api.entities.Activity;
@@ -13,29 +14,30 @@ import net.dv8tion.jda.api.interactions.components.text.TextInput;
 import net.dv8tion.jda.api.interactions.components.text.TextInputStyle;
 import net.dv8tion.jda.api.interactions.modals.Modal;
 import net.dv8tion.jda.api.requests.GatewayIntent;
+import net.dv8tion.jda.api.utils.ChunkingFilter;
+import net.dv8tion.jda.api.utils.MemberCachePolicy;
 import org.bukkit.Bukkit;
-import org.bukkit.entity.Player;
+import org.bukkit.OfflinePlayer;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.List;
 
 public final class WhitelistAddBot extends JavaPlugin {
 
     @Override
     public void onEnable() {
+        saveDefaultConfig();
 
-        Collection<GatewayIntent> intents = new ArrayList<>();
-        intents.add(GatewayIntent.GUILD_MESSAGES);
-        intents.add(GatewayIntent.MESSAGE_CONTENT);
-
-        JDABuilder builder = JDABuilder.create(getConfig().getString("token"), intents);
+        JDABuilder builder = JDABuilder.createDefault(getConfig().getString("token"));
         builder.setStatus(OnlineStatus.ONLINE);
         builder.setActivity(Activity.competing("ホワイトリスト"));
-        builder.addEventListeners(new Listener());
-        builder.build();
+        builder.enableIntents(GatewayIntent.GUILD_MEMBERS, GatewayIntent.MESSAGE_CONTENT);
+        builder.setMemberCachePolicy(MemberCachePolicy.ALL);
+        builder.setChunkingFilter(ChunkingFilter.ALL);
+        JDA jda = builder.build();
+        jda.addEventListener(new Listener());
     }
 
     @Override
@@ -45,46 +47,50 @@ public final class WhitelistAddBot extends JavaPlugin {
     private static class Listener extends ListenerAdapter {
         @Override
         public void onGuildReady(@NotNull GuildReadyEvent event) {
-            super.onGuildReady(event);
-
-            List<CommandData> commandData = new ArrayList<>();
-            commandData.add(Commands.slash("whitelist", "ホワイトリストにプレイヤーを追加します"));
-            event.getGuild().updateCommands().addCommands(commandData).queue();
+            if (event.getGuild().getId().equals("1087726461315731510")) {
+                List<CommandData> commandData = new ArrayList<>();
+                commandData.add(Commands.slash("whitelist", "ホワイトリストにプレイヤーを追加します"));
+                event.getGuild().updateCommands().addCommands(commandData).queue();
+            }
         }
 
         @Override
         public void onSlashCommandInteraction(@NotNull SlashCommandInteractionEvent event) {
             super.onSlashCommandInteraction(event);
 
-            if (event.getName().equals("whitelist")) {
-                TextInput playerNameInput = TextInput.create("playerNameInput", "プレイヤー名", TextInputStyle.SHORT)
-                        .setRequired(true)
-                        .setMinLength(1)
-                        .setValue(event.getUser().getName())
-                        .build();
+            if (event.getChannel().getId().equals("1088773480754982932")) {
+                if (event.getName().equals("whitelist")) {
+                    TextInput playerNameInput = TextInput.create("playerName", "プレイヤー名", TextInputStyle.SHORT)
+                            .setRequired(true)
+                            .setMinLength(1)
+                            .setValue(event.getUser().getName())
+                            .build();
 
-                Modal modal = Modal.create("whitelistModal", "プレイヤーを追加する")
-                        .setTitle("プレイヤーを追加する")
-                        .addActionRow(playerNameInput)
-                        .build();
+                    Modal modal = Modal.create("whitelistModal", "プレイヤーを追加する")
+                            .setTitle("プレイヤーを追加する")
+                            .addActionRow(playerNameInput)
+                            .build();
 
-                event.replyModal(modal).queue();
+                    event.replyModal(modal).queue();
+                }
             }
         }
 
+        @SuppressWarnings("deprecation")
         @Override
         public void onModalInteraction(@NotNull ModalInteractionEvent event) {
             super.onModalInteraction(event);
 
             if (event.getModalId().equals("whitelistModal")) {
-                Player player = Bukkit.getPlayer(String.valueOf(event.getInteraction().getValue("playerNameInput")));
+                OfflinePlayer player = Bukkit.getOfflinePlayer(event.getInteraction().getValue("playerName").getAsString());
                 if (player == null) {
-                    event.reply("プレイヤーIDが異なるため、正常に登録ができませんでした。").queue();
+                    event.reply("プレイヤーIDが異なるため、正常に登録ができませんでした。").setEphemeral(true).queue();
                 } else {
                     if (player.isWhitelisted()) {
-                        event.reply("そのプレイヤーはすでに登録されています").queue();
+                        event.reply("そのプレイヤーはすでに登録されています").setEphemeral(true).queue();
                     } else {
                         player.setWhitelisted(true);
+                        event.reply("登録しました。").setEphemeral(true).queue();
                     }
                 }
             }
